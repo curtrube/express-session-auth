a632460944858b255804c318e8d93cbf8c6026cb
Curtis Rubeck <rubeckcurtis@gmail.com>
2024-03-13 09:17:43 -0500

    feat: implements session cookies

--- app.js
+++ app.js
@@ -1,31 +1,67 @@
 const express = require("express");
-const session = require("express-session");
+const cookieParser = require('cookie-parser')
 
 const app = express();
 
+app.set('view engine', 'ejs')
+
+app.use(cookieParser())
+
 app.use(express.json());
 
-app.use(
-  session({
-    secret: "SJHWCuBgl5XJW5Yz}gRbWF2S?dK4PZqYU^+>e<?Y?Adf68?ldW",
-    cookie: { maxAge: 30000 },
-    saveUninitialized: false,
-    resave: false,
-  })
-);
+app.use(express.urlencoded({ extended: true }));
+
+
+// simulate user database
+
+const USERS = new Map()
+
+USERS.set('curtis', { id: 1, username: 'curtis', password: 'supersecret' })
+USERS.set('alex', { id: 2, username: 'alex', password: 'abc123' })
+
+const SESSIONS = new Map()
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "hello world" });
 });
 
+app.get('/login', (req, res) => {
+  res.render('login', { error: null })
+})
+
 app.post("/login", (req, res) => {
-  console.log(req.sessionID);
   const { username, password } = req.body;
-  if (!username || !password) {
-    res.status(401).json({ message: "you must login" });
+  const user = USERS.get(username)
+  if (!user) {
+    res.status(401).json({ message: `no user found matching ${username}` })
+    return;
+  } else {
+    console.log(`found user ${username}`)
   }
-  res.status(200).json({ message: "hello login route" });
+
+  // create unique session id
+  const sessionId = crypto.randomUUID();
+  SESSIONS.set(sessionId, username)
+
+  res.cookie('sessionId', sessionId, {
+    secure: true,
+    httpOnly: true,
+    sameSite: "strict",
+    // maxAge: 30000
+  }).send(`Authenticated as ${username}`)
 });
+
+app.get('/private', (req, res) => {
+  console.log(req.cookies)
+  const user = SESSIONS.get(req.cookies.sessionId)
+  console.log(user)
+  if (user == null) {
+    res.sendStatus(401)
+    return;
+  }
+  res.status(200).json({ message: 'super secret data only authenticatd users can view' })
+  // res.status(200).send('sensative data')
+})
 
 app.listen(3000, () => {
   console.log("app listening on port 3000");
